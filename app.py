2e40ae1e94e93444bb77325c15bd28e023054e52
Óscar Miranda <oscarmiranda3615@gmail.com>
2020-11-02 16:26:43 -0700

    Propagar excepciones, evita error 500

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ from db import db
 
 app = Flask(__name__)
 
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///Data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
